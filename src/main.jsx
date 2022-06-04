97b57b484f1bb9c2fe061288a551d272766af9b3
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2022-06-04 10:32:05 -0500

    followers component and repository refactored

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './scss/styles.scss';
-// import './scss/index.scss';
 
 const root = createRoot(document.getElementById('root'));
 
